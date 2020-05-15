e78d478df9cc8ff73f581d79365e182713c3108c
zhouhaoUpUp <171245293@qq.com>
2020-05-15 12:16:51 +0800

    1

--- .tea/entryFiles-development/index$.web.js
+++ .tea/entryFiles-development/index$.web.js
@@ -2,3 +2,4 @@ require('@alipay/appx-compiler/lib/sjsEnvInit');
 require('./config$');
 
 require('../../pages/index/index?hash=5158fa18297db3fbaac119609b168d20fcdf1eea');
+require('../../pages/orderFormList/affirmList/affirmList?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
