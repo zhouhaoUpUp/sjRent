d29b277f840b401aa4745ffc41245f8bc1c67e69
unknown <lsl632@aliyun.com>
2020-05-15 12:13:35 +0800

    jiazi

--- .tea/entryFiles-development/index$.worker.js
+++ .tea/entryFiles-development/index$.worker.js
@@ -30,6 +30,8 @@ if(AFAppX.registerApp) {
 function success() {
 require('../../app');
 require('../../pages/index/index?hash=5158fa18297db3fbaac119609b168d20fcdf1eea');
+require('../../pages/orderForm/orderForm?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/myMessage/myMessage?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
 }
