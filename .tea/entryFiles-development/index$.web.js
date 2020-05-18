20c38b79dd9eece17866871a539d281f8bca8ac9
zhouhaoUpUp <171245293@qq.com>
2020-05-18 09:36:36 +0800

    5.18

--- .tea/entryFiles-development/index$.web.js
+++ .tea/entryFiles-development/index$.web.js
@@ -1,6 +1,7 @@
 require('@alipay/appx-compiler/lib/sjsEnvInit');
 require('./config$');
 
+require('../../components/ordertypes/type-waitpay/type-waitpay?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
 require('../../pages/index/index?hash=5158fa18297db3fbaac119609b168d20fcdf1eea');
 require('../../pages/orderFormList/affirmList/affirmList?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
-require('../../pages/orderFormList/affirmWaitPay/affirmWaitPay?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
+require('../../pages/orderFormList/affirmWaitPay/affirmWaitPay?hash=1cfce0a3ea53fa9363003158bea9459ab7376841');
